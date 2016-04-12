da32a269a284d66ca48bb216fc5283909ee16f83
Carlos Poles <carlos.poles@griffithuni.edu.au>
2016-04-12 13:59:18 +1000

    Added Contact List Class

---
+++ ContactList/ContactList.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList.swift
+//  ContactList
+//
+//  Created by Carlos Poles on 12/04/2016.
+//  Copyright © 2016 Carlos Poles. All rights reserved.
+//
+
+import Foundation
+
+class ContactList {
+    
+    // MARK: - Properties
+    
+    var entries: [ContactListEntry] = []
+    
+}
